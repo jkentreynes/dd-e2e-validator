4594d2d076f7158fed6d25e0903349cb08730e2f
John Kent Reynes <johnkentreynes@gmail.com>
2015-11-03 08:20:35 +0800

    test(dd-e2e): create data driven validator

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+var Promise       = require( 'bluebird' );
+var jsondiffpatch = require('jsondiffpatch');
+var handlebars    = require( 'handlebars' );
+var fs            = require( 'fs-extra' );
+var source        = fs.readFileSync( './templates/visual-diff.html', 'utf8' );
+
+
+function validate ( json1, json2, output ) {
+
+	return new Promise ( function ( resolve, reject ) {
+
+		var filename = json1.scenarioId + json1.scenario;
+
+		var diff = jsondiffpatch.diff( json1.data, json2 );
+
+		if ( !diff ) {
+			json1 = JSON.stringify( json1.data );
+			json2 = JSON.stringify( json2 );
+
+			var context = {
+						'json1' : json1,
+						'json2' : json2
+					};
+
+			var template = handlebars.compile( source );
+
+			var html = template( context );
+
+			fs.outputFileSync(
+				path.join(  output, filename + '.html' ), html
+				);
+
+			resolve( 'JSONs are not the same. See logs ' + path.join(  output, filename + '.html' ) + ' for details.' );
+		} else {
+			resolve( 'Both JSONs are the same.' );
+		}
+
+	} );
+};
+
+module.exports = validate();

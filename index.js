766b4abc74c767e0634420f4f09e9ac4de124a75
John Kent Reynes <johnkentreynes@gmail.com>
2015-11-03 08:20:35 +0800

    test(dd-e2e): create data driven validator

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var Promise       = require( 'bluebird' );
 var jsondiffpatch = require('jsondiffpatch');
 var handlebars    = require( 'handlebars' );
@@ -8,35 +9,32 @@ var source        = fs.readFileSync( './templates/visual-diff.html', 'utf8' );
 
 function validate ( json1, json2, output ) {
 
-	return new Promise ( function ( resolve, reject ) {
+	var filename = json1.scenarioId + json1.scenario;
 
-		var filename = json1.scenarioId + json1.scenario;
+	var diff = jsondiffpatch.diff( json1.data, json2 );
 
-		var diff = jsondiffpatch.diff( json1.data, json2 );
+	if ( !diff ) {
+		json1 = JSON.stringify( json1.data );
+		json2 = JSON.stringify( json2 );
 
-		if ( !diff ) {
-			json1 = JSON.stringify( json1.data );
-			json2 = JSON.stringify( json2 );
+		var context = {
+					'json1' : json1,
+					'json2' : json2
+				};
 
-			var context = {
-						'json1' : json1,
-						'json2' : json2
-					};
+		var template = handlebars.compile( source );
 
-			var template = handlebars.compile( source );
+		var html = template( context );
 
-			var html = template( context );
+		fs.outputFileSync(
+			path.join(  output, filename + '.html' ), html
+			);
 
-			fs.outputFileSync(
-				path.join(  output, filename + '.html' ), html
-				);
+		return( 'JSONs are not the same. See logs ' + path.join(  output, filename + '.html' ) + ' for details.' );
+	} else {
+		return( 'Both JSONs are the same.' );
+	}
 
-			resolve( 'JSONs are not the same. See logs ' + path.join(  output, filename + '.html' ) + ' for details.' );
-		} else {
-			resolve( 'Both JSONs are the same.' );
-		}
-
-	} );
 };
 
 module.exports = validate();
